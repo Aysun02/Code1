fe38b457320b7cef72b3854fd138de25c23697d3
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-06-10 12:02:37 +0400

    Add files via upload

---
+++ Sealed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(ObjectMethod(23.89));
+        }
+        public static  object  ObjectMethod(object obj)
+        {
+            return obj;
+        }
+
+    }
+}
